3e8f2723fb7fcd87d33aecda994fa5a373bea139
stuti006 <stutishrivastava006@gmail.com>
2021-09-08 17:31:37 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SK')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','*']
+ALLOWED_HOSTS = ['127.0.0.1','https://appointmentdr.herokuapp.com/']
 
 
 
